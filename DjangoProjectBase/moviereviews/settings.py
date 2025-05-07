2bddf25cb0ec12b764f17457e5c4d00113a2da91
alexahurtado08 <ahurtadod@eafit.edu.co>
2025-05-07 10:33:21 -0500

    IP

--- DjangoProjectBase/moviereviews/settings.py
+++ DjangoProjectBase/moviereviews/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-+9jg)*p09$at+zck$n-6*djf=34w4sc*m##^fmph4pb$_nh%xb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.196.170.218']
+ALLOWED_HOSTS = ['3.84.194.78']
 
 
 # Application definition
